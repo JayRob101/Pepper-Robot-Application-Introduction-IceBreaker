e4a33cc9c588a1a35618d74cd9a0a98623ca7fc3
Sabancı MLT Lab <81460455+mindlangtech@users.noreply.github.com>
2021-06-21 16:47:40 +0300

    first commit

---
+++ Pepper Application/translations/translation_en_US.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Pure_Final/behavior_1/behavior.xar:/turkish conversation/Animated Say</name>
+        <message>
+            <location filename="Pure_Final/behavior_1/behavior.xar" line="0"/>
+            <source>Do you speek English? Can you please say yes or no?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Do you speek English? Can you please say yes or no?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Do you speek English? Can you please say yes or no?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Do you speek English? Can you please say yes or no?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/turkish conversation/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Do you speek English? Can you please say yes or no?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Do you speek English? Can you please say yes or no?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/turkish/Animated Say</name>
+        <message>
+            <source>Do you speek English? Can you please say yes or no?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Do you speek English? Can you please say yes or no?</translation>
+        </message>
+    </context>
+</TS>
